1af74d0539eb264cb025f6c11520273283d63806
ruMarcy6 <32559788+ruMarcy6@users.noreply.github.com>
2017-11-16 00:39:21 -0500

    Update newpthread_tester.c

--- newpthread_tester.c
+++ newpthread_tester.c
@@ -1,70 +1,74 @@
 #include "my_pthread.c"
 #include <stdio.h>
 
-/* this function is run by the second thread */
-void *inc_x(void *x_void_ptr){
+my_pthread_mutex_t xlock;
+my_pthread_mutex_t ylock;
+
+void* inc_x(void *x_void_ptr){
 
 	/* increment x to 100 */
 	int *x_ptr = (int *)x_void_ptr;
-	while(++(*x_ptr) < 100);
+	int i = 0;
+	while(i<100){
+		my_pthread_mutex_lock(&xlock);
+		(*x_ptr)++;
+		my_pthread_mutex_unlock(&xlock);
+		i++;
+	};
 
 int count=0;
- while(1)
+int running=0;
+	
+	while(running<(MEMORY_SIZE/PAGE_SIZE)+10)
         {
-        count++;
-                void *m = malloc(1024*1024);
-                memset(m,0,1024*1024);
-                printf("This is the amount of times it's gone: %d\n", count); 
+		running++;
+                void *m = malloc(PAGE_SIZE-10);
+                memset(m,0,PAGE_SIZE-10);
+		printf("This is the current count for malloc: %d", count);
         }
-	printf("x increment finished\n");
-
-	/* the function must return something - NULL will do */
+  
+	
 	return NULL;
 }
 
+void* inc_y(void *y_void_ptr){
 
-int main(){
+	/* increment y to 100 */
+	int *y_ptr = (int *)y_void_ptr;
+	int i = 0;
+	while(i<100){
+		my_pthread_mutex_lock(&ylock);
+		(*y_ptr)++;
+		my_pthread_mutex_unlock(&ylock);
+		i++;
+	};
 
-	int x = 0, y = 0;
-
-	/* show the initial values of x and y */
-	printf("x: %d, y: %d\n", x, y);
-
-	/* this variable is our reference to the second thread */
-	my_pthread_t inc_x_thread;
-
-	/* create a second thread which executes inc_x(&x) */
-	int ret = my_pthread_create(&inc_x_thread, NULL, inc_x, &x);
-	if(ret != 0) {
-		fprintf(stderr, "Error creating thread\nError code: %d\n", ret);
-		return 1;
-	}
-	/* increment y to 100 in the first thread */
-	while(++y < 100);
-
-	printf("y increment finished\n");
-
-	/* wait for the second thread to finish */
-	printf(" In the end of the thread: x: %d, y: %d\n", x, y);
-	ret = my_pthread_join(inc_x_thread, NULL);
-	printf(" In the end of the thread: x: %d, y: %d\n", x, y);
-	
-	if(ret != 0) {
-		fprintf(stderr, "Error joining thread\nError Code: %d\n", ret);
-		return 2;
-	}
-
-	/* show the results - x is now 100 thanks to the second thread */
-	printf("x: %d, y: %d\n", x, y);
-
-	return 0;
+	return NULL;
 }
 
+int main(){
+	int THREAD_NUM = 20;
+	my_pthread_t threads[THREAD_NUM];
 
-/*
-This code SHOULD print something like:
-x: 0, y:0
-y increment finished
-x increment finished
-x: 100, y: 100
-*/
+	int x = 0, y = 0;
+	my_pthread_mutex_init(&xlock, NULL);
+	my_pthread_mutex_init(&ylock, NULL);
+
+	int i;
+
+	for(i=0; i<THREAD_NUM; i+=2){
+		my_pthread_create(&threads[i], NULL, &inc_x, &x);
+		my_pthread_create(&threads[i+1], NULL, &inc_y, &y);
+	}
+
+	for(i=0; i<THREAD_NUM; i++){
+		my_pthread_join(threads[i], NULL);
+	}
+
+	my_pthread_mutex_destroy(&xlock);
+	my_pthread_mutex_destroy(&ylock);
+
+	printf("Final values: x: %d y: %d\n", x, y);	//1000, 1000
+	
+	return 0;
+}
