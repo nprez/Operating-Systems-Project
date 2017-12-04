71b2e3d6a3c5dcc93e02e42f3e87d8860b9db3a3
Calmas <energizer412@gmail.com>
2017-12-03 19:55:02 -0500

    added skeleton of required asst3 functions

---
+++ filesystem.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct dirent{
+  int abc;
+};
+
+struct stat{
+  int abc;
+};
+
+int create(char* path){
+  return 0;
+}
+
+int delete(char* path){
+  return 0;
+}
+
+int stat(char* path, struct stat* buf){
+  return 0;
+}
+
+int open(char* path){
+  return 0;
+}
+
+int close(int fileID){
+  return 0;
+}
+
+int read(int fileID, void* buffer, int bytes){
+  return 0;
+}
+
+int write(int fileID, void* buffer, int bytes){
+  return 0;
+}
+
+struct dirent* readdir(int directoryID){
+  return NULL;
+}
