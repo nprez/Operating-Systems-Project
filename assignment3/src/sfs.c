3132fc07f2a4e81c68eef02993cde1f6b5b7a45a
Calmas <energizer412@gmail.com>
2017-12-11 18:47:05 -0500

    init stuff

--- assignment3/src/sfs.c
+++ assignment3/src/sfs.c
@@ -37,7 +37,7 @@
 typedef struct block_{
   int type;
   stat s;
-  struct block_* p[];
+  struct block_* p[(512 - (sizeof(stat)+sizeof(int)))/sizeof(struct block_*)];
 
   /*
   dev_t     st_dev   Device ID of device containing file.
@@ -60,6 +60,7 @@ typedef struct block_{
   */
 } block;
 
+
 ///////////////////////////////////////////////////////////
 //
 // Prototypes for all these functions, and the C-style comments,
@@ -80,8 +81,29 @@ void *sfs_init(struct fuse_conn_info *conn){
   fprintf(stderr, "in bb-init\n");
   log_msg("\nsfs_init()\n");
 
-	//open disk
-	open_disk(sfs_data->diskfile);
+  //open disk
+  open_disk(sfs_data->diskfile);
+
+  block* newBlock = (block*)malloc(sizeof(block));
+  newBlock->p = NULL;
+  newBlock->type = -1;
+  newBlock->stat->st_dev = 0;
+  newBlock->stat->st.ino = 0;
+  newBlock->stat->st_mode = 0;
+  newBlock->stat->st_nlink = 0;
+  newBlock->stat->st_uid = 0;
+  newBlock->stat->st_gid = 0;
+  newBlock->stat->st_rdev = 0;
+  newBlock->stat->st_size = 0;
+  newBlock->stat->st_blksize = 0;
+  newBlock->stat->st_blocks = 0;
+  newBlock->stat->st_atime = 0;
+  newBlock->stat->st_mtime = 0;
+  newBlock->stat->st_ctime = 0;
+
+  int i;
+  for(i = 0; i <= (FileSize/BlockSize);i++)
+    block_write(i,newBlock);
 
   log_conn(conn);
   log_fuse_context(fuse_get_context());
